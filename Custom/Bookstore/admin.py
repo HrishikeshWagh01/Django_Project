68930605aa0a6535befb6b6bace864134dc10c88
Hrishkesh <hrishikeshwagh2001@gmail.com>
2021-09-23 12:16:13 +0530

    Initial commit

---
+++ Custom/Bookstore/admin.py
@@ -0,0 +1,25 @@
+from django.db import models
+from  django.contrib.auth.models  import  Group
+from django.contrib import admin
+from .models import Category, Publisher ,Details, Author
+
+class  categoryAdmin(admin.ModelAdmin):
+    pass
+
+class  publisherAdmin(admin.ModelAdmin):
+    pass
+
+
+class  detailsAdmin(admin.ModelAdmin):
+    list_display = ('book_name', 'category', 'Author', 'pages', 'publisher')
+
+class  authorAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, categoryAdmin)
+admin.site.register(Publisher, publisherAdmin)
+admin.site.register(Details, detailsAdmin)
+admin.site.register(Author, authorAdmin)
+
+admin.site.unregister(Group)
+
